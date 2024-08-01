243056343e7bbf8e517cc7e831130fa51c1a8115
tfd-ed <info@tfdevs.com>
2024-08-01 14:56:53 +0700

    Hello3

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello, World 2!');
+  res.status(200).send('Hello, World 3!');
 });
 
 app.get('/user/:id', (req, res) => {
