4396fcf43098280f9e51a9c526f39a3c06849489
Songhay <soksonghay76@gmail.com>
2024-08-01 15:56:26 +0700

    Hello world

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello, World songhay!');
+  res.status(200).send('Hello, World songhay koko love whit hello!');
 });
 
 app.get('/user/:id', (req, res) => {
