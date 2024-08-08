8547f97cffa5a0b63a5ed66da83c3e8c763580a5
songhay168 <soksonghay76@gmail.com>
2024-08-08 12:57:45 +0700

    first CI/CD 168

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello, World Mirira!');
+  res.status(200).send('Hello, World Mirira168!');
 });
 
 app.get('/user/:id', (req, res) => {
